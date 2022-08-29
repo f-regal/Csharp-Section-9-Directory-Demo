b129b6647dedbcd11a4286dd4a5a0214b07a27fd
Faysal Regal <faysalregal@gmail.com>
2022-08-29 18:09:16 +0100

    used directory and directoryInfo classes

--- Csharp-Section-9-Directory-Demo/Program.cs
+++ Csharp-Section-9-Directory-Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CsharpSection9DirectoryDemo
 {
@@ -6,7 +7,32 @@ namespace CsharpSection9DirectoryDemo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Directory.CreateDirectory(@"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Demo/Path1/");
+
+            //var files = Directory.GetFiles(@"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/", "*.sln", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file);
+            //}
+
+            var directories = Directory.GetDirectories(@"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/", "*.*", SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("...");
+
+
+            // DIRECTORY INFO
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+
+
+
         }
     }
 }
